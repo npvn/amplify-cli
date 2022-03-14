f0bfb965fa9e98edddd381e4ed5f8122ee8669a6
John Corser <xss@amazon.com>
2022-03-14 15:49:43 +0000

    fix: ensure exeInfo is set befort using it's spinner

--- packages/amplify-category-function/src/provider-utils/awscloudformation/secrets/ssmClientWrapper.ts
+++ packages/amplify-category-function/src/provider-utils/awscloudformation/secrets/ssmClientWrapper.ts
@@ -104,7 +104,10 @@ export class SSMClientWrapper {
 
 const getSSMClient = async (context: $TSContext): Promise<aws.SSM> => {
   const spinner = ora('Initializing SSM Client');
-  context.exeInfo.spinner = spinner;
+  if (context.exeInfo) {
+    context.exeInfo.spinner = spinner;
+  }
+
   try {
     spinner.start();
 
