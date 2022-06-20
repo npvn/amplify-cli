15a50c518c2b7101b6d912cbd6dd7253f05e89f6
Phuc Nguyen <contact@phucnguyen.info>
2022-06-20 17:31:12 +0700

    Custom websocket keep alive interval

--- packages/amplify-appsync-simulator/src/server/subscription/websocket-server/server.ts
+++ packages/amplify-appsync-simulator/src/server/subscription/websocket-server/server.ts
@@ -16,7 +16,7 @@ import { MESSAGE_TYPES } from './message-types';
 import { decodeHeaderFromQueryParam } from './utils';
 
 const PROTOCOL = 'graphql-ws';
-const KEEP_ALIVE_TIMEOUT = 4 * 60 * 1000; // Wait time between Keep Alive Message
+const KEEP_ALIVE_TIMEOUT = 60 * 1000; // Wait time between Keep Alive Message
 // Max time the client will wait for Keep Alive message before disconnecting. Sent to the client as part of connection ack
 const CONNECTION_TIMEOUT_DURATION = 5 * 60 * 1000;
 
